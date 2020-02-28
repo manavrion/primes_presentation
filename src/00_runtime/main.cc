c52f9681ea5fd9ea283cc07478ffe9dbaeca55bf
Ruslan Manaev <manavrion@gmail.com>
2020-02-28 10:28:30 +0300

    Implement details

---
+++ src/00_runtime/main.cc
@@ -0,0 +1,42 @@
+#include <array>
+#include <cassert>
+#include <vector>
+
+auto get_primes_impl(size_t limit) {
+  std::vector<bool> not_prime(limit);
+  not_prime[0] = not_prime[1] = true;
+
+  for (size_t i = 2; i < not_prime.size(); ++i) {
+    if (!not_prime[i]) {
+      if (i * i < not_prime.size()) {
+        for (size_t j = i * i; j < not_prime.size(); j += i) {
+          not_prime[j] = true;
+        }
+      }
+    }
+  }
+  return not_prime;
+}
+
+std::vector<size_t> get_primes(size_t limit) {
+  std::vector<size_t> primes;
+  auto not_prime = get_primes_impl(limit);
+
+  auto it = primes.begin();
+  for (size_t i = 0; i < not_prime.size(); ++i) {
+    if (!not_prime[i]) {
+      primes.emplace_back(i);
+    }
+  }
+  return primes;
+}
+
+int main() {
+  std::vector<size_t> primes = get_primes(1000);
+  assert(primes[0] == 2);
+  assert(primes[1] == 3);
+  assert(primes[2] == 5);
+  assert(primes[3] == 7);
+
+  return 0;
+}
