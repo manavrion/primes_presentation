b3280058c1bddf9362cee080766d86ad544491dd
Ruslan Manaev <manavrion@gmail.com>
2020-11-09 00:29:50 +0500

    Improve CI

--- toolchain/clang_release.cmake
+++ toolchain/clang_release.cmake
@@ -9,7 +9,7 @@ set(CMAKE_NM llvm-nm-12)
 set(CMAKE_OBJDUMP llvm-objdump-12)
 set(CMAKE_RANLIB llvm-ranlib-12)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -fconstexpr-depth=1000 -fconstexpr-steps=1000000000")
 
 # Build
 set(CMAKE_BUILD_TYPE Release)
