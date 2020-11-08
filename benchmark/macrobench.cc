746309ee88236ea4461ca14a4ed0a37e652f26a5
Ruslan Manaev <manavrion@gmail.com>
2020-11-09 00:22:53 +0500

    Improve CI

--- benchmark/macrobench.cc
+++ benchmark/macrobench.cc
@@ -45,8 +45,10 @@ std::optional<std::int64_t> try_build(const size_t table_limit,
   if (status == 0) {
     std::cout << duration / 1000 / 1000 << "ms \t";
   } else {
-    std::cout << "compilation failed \n";
+    std::cout << "compilation failed \t";
   }
+
+  std::cout << (is_debug ? "debug" : "release") << "\t";
 
   std::cout.flush();
   system(du_command.c_str());
@@ -101,8 +103,69 @@ std::optional<std::int64_t> try_build_02_build_step(const size_t table_limit,
   if (status == 0) {
     std::cout << duration / 1000 / 1000 << "ms \t";
   } else {
-    std::cout << "compilation failed \n";
+    std::cout << "compilation failed \t";
   }
+
+  std::cout << (is_debug ? "debug" : "release") << "\t";
+
+  std::cout.flush();
+  system(du_command.c_str());
+
+  if (status == 0) {
+    return duration;
+  }
+  return std::nullopt;
+}
+
+std::optional<std::int64_t> try_build_02_build_step_gen_without_preprare_step(
+    const size_t table_limit, bool is_debug = false) {
+  const std::string target = "02_build_step";
+  std::string compile_command = "clang++-12";
+  compile_command += " -stdlib=libc++";
+  compile_command += " -std=c++20";
+  compile_command += " -fconstexpr-depth=1000";
+  compile_command += " -fconstexpr-steps=1000000000";
+  compile_command += " ../src/" + target + "/main.cc";
+  compile_command += " -o " + target;
+  compile_command += " -DTABLE_LIMIT=" + std::to_string(table_limit);
+  compile_command += " -O3";
+
+  std::string generation_command = "echo " + std::to_string(table_limit) +
+                                   " | ./02_build_step > "
+                                   "02_build_step_gen.cc";
+
+  std::string gen_compile_command = "clang++-12";
+  gen_compile_command += " -stdlib=libc++";
+  gen_compile_command += " -std=c++20";
+  gen_compile_command += " -fconstexpr-depth=1000";
+  gen_compile_command += " -fconstexpr-steps=1000000000";
+  gen_compile_command += " 02_build_step_gen.cc";
+  gen_compile_command += " -o 02_build_step_gen_without_preprare_step";
+  gen_compile_command += " -DTABLE_LIMIT=" + std::to_string(table_limit);
+  if (!is_debug) {
+    gen_compile_command += " -O3";
+  }
+
+  cmd(compile_command, false);
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  auto status2 = cmd(generation_command, false, false);
+  auto status3 = cmd(gen_compile_command, false);
+  auto t2 = std::chrono::high_resolution_clock::now();
+
+  auto status = status2 + status3;
+
+  std::string du_command = "du -h 02_build_step_gen_without_preprare_step";
+
+  auto duration =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+  if (status == 0) {
+    std::cout << duration / 1000 / 1000 << "ms \t";
+  } else {
+    std::cout << "compilation failed \t";
+  }
+
+  std::cout << (is_debug ? "debug" : "release") << "\t";
 
   std::cout.flush();
   system(du_command.c_str());
@@ -116,9 +179,14 @@ std::optional<std::int64_t> try_build_02_build_step(const size_t table_limit,
 void bench(const size_t table_limit) {
   std::cout << "CONFIGURATION with table_limit = " << table_limit << std::endl;
 
-  try_build(table_limit, "00_runtime");
-  try_build(table_limit, "01_compile_time");
-  try_build_02_build_step(table_limit);
+  try_build(table_limit, "00_runtime", false);
+  try_build(table_limit, "00_runtime", true);
+  try_build(table_limit, "01_compile_time", false);
+  try_build(table_limit, "01_compile_time", true);
+  try_build_02_build_step(table_limit, false);
+  try_build_02_build_step(table_limit, true);
+  try_build_02_build_step_gen_without_preprare_step(table_limit, false);
+  try_build_02_build_step_gen_without_preprare_step(table_limit, true);
 
   std::cout << std::endl;
 }
