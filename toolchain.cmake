dbebc53b93c850283a797fb07c0f536f67e4c841
Ruslan Manaev <manavrion@gmail.com>
2020-11-08 16:23:59 +0500

    Update compiler

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,13 +1,13 @@
 # Tools
-set(CMAKE_C_COMPILER clang-10)
+set(CMAKE_C_COMPILER clang-12)
 
-set(CMAKE_CXX_COMPILER clang++-10)
+set(CMAKE_CXX_COMPILER clang++-12)
 
-set(CMAKE_AR llvm-ar-10)
-set(CMAKE_LINKER llvm-link-10)
-set(CMAKE_NM llvm-nm-10)
-set(CMAKE_OBJDUMP llvm-objdump-10)
-set(CMAKE_RANLIB llvm-ranlib-10)
+set(CMAKE_AR llvm-ar-12)
+set(CMAKE_LINKER llvm-link-12)
+set(CMAKE_NM llvm-nm-12)
+set(CMAKE_OBJDUMP llvm-objdump-12)
+set(CMAKE_RANLIB llvm-ranlib-12)
 
 # Build
 set(CMAKE_BUILD_TYPE ${BUILD_TYPE})
